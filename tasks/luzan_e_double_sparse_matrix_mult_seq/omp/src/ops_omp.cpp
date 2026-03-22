2658b4e330b8d30affaa70a04026f5d433c65090
smallAbyss <eal1105@yandex.ru>
2026-03-22 21:19:51 +0000

    change pragma omp

--- tasks/luzan_e_double_sparse_matrix_mult_seq/omp/src/ops_omp.cpp
+++ tasks/luzan_e_double_sparse_matrix_mult_seq/omp/src/ops_omp.cpp
@@ -12,8 +12,9 @@ SparseMatrix SparseMatrix::CalcProdOMP(const SparseMatrix &b) const {
   /// tmp storage
   std::vector<std::vector<double>> values_per_col(b.cols_);
   std::vector<std::vector<unsigned>> rows_per_col(b.cols_);
+ int num_threads = ppc::util::GetNumThreads();
 
-#pragma omp parallel for shared(b, values_per_col, rows_per_col, kEPS) schedule(static) default(none) num_threads(2)
+#pragma omp parallel for shared(b, values_per_col, rows_per_col, kEPS) schedule(static) default(none) num_threads(num_threads)
   for (int b_col = 0; b_col < static_cast<int>(b.cols_); b_col++) {
     std::vector<double> tmp_col(rows_, 0.0);
 
