7f1821634789344b0862b1f4f42aa09713b857ab
smallAbyss <eal1105@yandex.ru>
2026-04-20 15:47:01 +0000

    [FIX] clang-format-fix

--- tasks/luzan_e_double_sparse_matrix_mult/tbb/src/ops_tbb.cpp
+++ tasks/luzan_e_double_sparse_matrix_mult/tbb/src/ops_tbb.cpp
@@ -1,13 +1,13 @@
-#include "luzan_e_double_sparse_matrix_mult/tbb/include/ops_tbb.hpp" 
+#include "luzan_e_double_sparse_matrix_mult/tbb/include/ops_tbb.hpp"
 
 #include <cmath>
 #include <cstddef>
 #include <vector>
-#include <atomic>
 
 // #include <tbb/parallel_for.h>
 // #include <tbb/blocked_range.h>
 #include <tbb/tbb.h>
+
 #include "luzan_e_double_sparse_matrix_mult/common/include/common.hpp"
 
 namespace luzan_e_double_sparse_matrix_mult {
@@ -16,42 +16,39 @@ SparseMatrix LuzanEDoubleSparseMatrixMultTBB::CalcProdTBB(const SparseMatrix &a,
 
   std::vector<std::vector<double>> values_per_col(b.cols);
   std::vector<std::vector<unsigned>> rows_per_col(b.cols);
-tbb::enumerable_thread_specific<std::vector<double>> tls_tmp(
-    [&] { return std::vector<double>(a.rows, 0.0); });
+  tbb::enumerable_thread_specific<std::vector<double>> tls_tmp([&] { return std::vector<double>(a.rows, 0.0); });
 
-  tbb::parallel_for(
-      tbb::blocked_range<unsigned>(0, b.cols),
-      [&](const tbb::blocked_range<unsigned>& range) {
-// std::cout << tbb::this_task_arena::max_concurrency() << std::endl;
-        for (unsigned b_col = range.begin(); b_col < range.end(); ++b_col) {
-          // std::vector<double> tmp_col(a.rows, 0.0);
-          
-          auto& tmp_col = tls_tmp.local();
-          std::fill(tmp_col.begin(), tmp_col.end(), 0.0);
+  tbb::parallel_for(tbb::blocked_range<unsigned>(0, b.cols), [&](const tbb::blocked_range<unsigned> &range) {
+    // std::cout << tbb::this_task_arena::max_concurrency() << std::endl;
+    for (unsigned b_col = range.begin(); b_col < range.end(); ++b_col) {
+      // std::vector<double> tmp_col(a.rows, 0.0);
 
-          unsigned b_start = b.col_index[b_col];
-          unsigned b_end   = b.col_index[b_col + 1];
+      auto &tmp_col = tls_tmp.local();
+      std::fill(tmp_col.begin(), tmp_col.end(), 0.0);
 
-          for (unsigned b_pos = b_start; b_pos < b_end; b_pos++) {
-            double b_val = b.value[b_pos];
-            unsigned k = b.row[b_pos];
+      unsigned b_start = b.col_index[b_col];
+      unsigned b_end = b.col_index[b_col + 1];
 
-            unsigned a_start = a.col_index[k];
-            unsigned a_end   = a.col_index[k + 1];
+      for (unsigned b_pos = b_start; b_pos < b_end; b_pos++) {
+        double b_val = b.value[b_pos];
+        unsigned k = b.row[b_pos];
 
-            for (unsigned a_pos = a_start; a_pos < a_end; a_pos++) {
-              tmp_col[a.row[a_pos]] += a.value[a_pos] * b_val;
-            }
-          }
+        unsigned a_start = a.col_index[k];
+        unsigned a_end = a.col_index[k + 1];
 
-          for (unsigned i = 0; i < a.rows; i++) {
-            if (fabs(tmp_col[i]) > kEPS) {
-              values_per_col[b_col].push_back(tmp_col[i]);
-              rows_per_col[b_col].push_back(i);
-            }
-          }
+        for (unsigned a_pos = a_start; a_pos < a_end; a_pos++) {
+          tmp_col[a.row[a_pos]] += a.value[a_pos] * b_val;
         }
-      });
+      }
+
+      for (unsigned i = 0; i < a.rows; i++) {
+        if (fabs(tmp_col[i]) > kEPS) {
+          values_per_col[b_col].push_back(tmp_col[i]);
+          rows_per_col[b_col].push_back(i);
+        }
+      }
+    }
+  });
 
   c.col_index.push_back(0);
   for (unsigned j = 0; j < b.cols; j++) {
--- tasks/luzan_e_double_sparse_matrix_mult/tests/functional/main.cpp
+++ tasks/luzan_e_double_sparse_matrix_mult/tests/functional/main.cpp
@@ -72,11 +72,10 @@ const std::array<TestType, 6> kTestParam = {std::make_tuple("test_1.txt", "01"),
                                             std::make_tuple("test_3.txt", "03"), std::make_tuple("test_4.txt", "04"),
                                             std::make_tuple("test_5.txt", "05"), std::make_tuple("test_6.txt", "06")};
 
-const auto kTestTasksList = std::tuple_cat(
-  ppc::util::AddFuncTask<LuzanEDoubleSparseMatrixMultSeq, InType>(
-    kTestParam, PPC_SETTINGS_luzan_e_double_sparse_matrix_mult),
-  ppc::util::AddFuncTask<LuzanEDoubleSparseMatrixMultTBB, InType>(
-    kTestParam, PPC_SETTINGS_luzan_e_double_sparse_matrix_mult));
+const auto kTestTasksList = std::tuple_cat(ppc::util::AddFuncTask<LuzanEDoubleSparseMatrixMultSeq, InType>(
+                                               kTestParam, PPC_SETTINGS_luzan_e_double_sparse_matrix_mult),
+                                           ppc::util::AddFuncTask<LuzanEDoubleSparseMatrixMultTBB, InType>(
+                                               kTestParam, PPC_SETTINGS_luzan_e_double_sparse_matrix_mult));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
