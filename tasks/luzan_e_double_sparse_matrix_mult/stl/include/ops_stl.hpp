a53d71dfcabc6831cd29e03db4ccb1efbe526e6b
smallAbyss <eal1105@yandex.ru>
2026-05-03 22:41:01 +0000

    [DEV] stl done

---
+++ tasks/luzan_e_double_sparse_matrix_mult/stl/include/ops_stl.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "luzan_e_double_sparse_matrix_mult/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace luzan_e_double_sparse_matrix_mult {
+
+class LuzanEDoubleSparseMatrixMultStl : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kSTL;
+  }
+  explicit LuzanEDoubleSparseMatrixMultStl(const InType &in);
+
+    static SparseMatrix CalcProdSTL(const SparseMatrix &a, const SparseMatrix &b);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace luzan_e_double_sparse_matrix_mult
